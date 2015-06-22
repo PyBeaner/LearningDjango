56372bd973f3c028c022c7f85c4767935dcb7df2
PyBeaner <758046738@qq.com>
2015-06-23 00:03:14 +0800

    Lookup API

--- mysite/step-by-step/step3.py
+++ mysite/step-by-step/step3.py
@@ -21,5 +21,21 @@ q.id
 
 Question.objects.all()
 [<Question: Question object>]
+
+
+# Lookup API
+Question.objects.filter(question_text__startswith='What')
+from django.utils import timezone
+current_year = timezone.now().year
+Question.objects.get(pub_date__year=current_year)
+
+# use pk(PrimaryKey) instead of id,pk is always available
+Question.objects.get(pk=1)
+
+# create objects
+q.choice_set.create(choice_text="The Sky",votes=1)
+
+# deeper lookup
+Choice.objects.filter(question__pub_date__startswith="What")
 """
 
