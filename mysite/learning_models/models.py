cebbaeb5b3ab1f058288d8fa53727357a35e3a02
XiaomingSu <xiaoming.su@patpat.com>
2015-06-24 13:15:06 +0800

    verbose_name and choices

--- mysite/learning_models/models.py
+++ mysite/learning_models/models.py
@@ -2,5 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField("person's first name", max_length=30)
+    last_name = models.CharField("person's last name", max_length=30)
+
+    SHIRT_SIZES = (
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
+    )
+    # within choices
+    shirt_size = models.CharField(choices=SHIRT_SIZES, max_length=1, default="S")
+    # p.shirt_size #M
+    # p.get_shirt_size_display() #Medium
